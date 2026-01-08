f26104979aa886227637edb7e937839e331a8c72
mine1x <sktm2k6@gmail.com>
2026-01-08 15:55:12 +0700

    refactor(run.bat): update variable definitions for improved portability

--- run.bat
+++ run.bat
@@ -1,12 +1,11 @@
 @echo off
 cls
 
-:: Define variables
-set "TARGET_FILE=C:\Users\hau\AppData\Roaming\Mindustry\mods\mindustrytoolmindustrytoolmod.zip"
-set "BUILD_TOOL=./gradlew jar"
-set "JAR_PATH=C:\Codes\MindustryTool\MindustryToolMod\build\libs\MindustryToolModDesktop.jar"
-set "DEST_FOLDER=C:\Users\nguyen\AppData\Roaming\Mindustry\mods"
-set "APP_TO_RUN=C:\Games\mindustry-windows-64-bit\Mindustry.exe"
+:: Define variables (use %APPDATA% and script directory)
+set "TARGET_FILE=%APPDATA%\Mindustry\mods\mindustrytoolmindustrytoolmod.zip"
+set "BUILD_TOOL=.\gradlew jar"
+set "JAR_PATH=%~dp0build\libs\MindustryToolModDesktop.jar"
+set "DEST_FOLDER=%APPDATA%\Mindustry\mods"
 
 :: Remove specific file if it exists
 if exist "%TARGET_FILE%" (
@@ -26,12 +25,14 @@ if not exist "%JAR_PATH%" (
     exit /b 1
 )
 
+:: Ensure destination folder exists
+if not exist "%DEST_FOLDER%" (
+    echo Destination folder "%DEST_FOLDER%" does not exist. Creating...
+    mkdir "%DEST_FOLDER%"
+)
+
 :: Copy JAR to destination folder
 echo Copying %JAR_PATH% to %DEST_FOLDER%...
 copy "%JAR_PATH%" "%DEST_FOLDER%" /y
-
-:: Run the specified application
-echo Running %APP_TO_RUN%...
-start "" "%APP_TO_RUN%"
 
 echo Done.
